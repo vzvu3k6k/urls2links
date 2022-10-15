0eb772ac9035f952bb07b4b8137e0d9eccb47fa3
vzvu3k6k <vzvu3k6k@gmail.com>
2022-10-15 22:59:02 +0900

    style

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ import (
 func main() {
 	var b strings.Builder
 
-	s := bufio.NewScanner(os.Stdin)
 	fmt.Fprint(&b, "<ul>")
+
+	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		t := s.Text()
 		fmt.Fprintf(&b, "<li><a href=\"%s\">%s</a>", t, t)
 	}
+
 	fmt.Fprint(&b, "</ul>")
 
 	fmt.Printf("data:text/html,%s", b.String())
